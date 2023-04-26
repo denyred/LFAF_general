6d430d51c30886dfe09b83aee4d90f86bb449d11
SPATARU DIONISIE <expert82822@gmail.com>
2023-04-26 14:54:58 +0300

    Lab 4

--- source/main.py
+++ source/main.py
@@ -1,22 +1,42 @@
 from grammar import Grammar
 from finite_automaton import FiniteAutomaton
 from Lexer import Lexer, Token
+import unittest
+from UnitTester import UnitTester
+from ChomskyConv import CNFConverter
+
 
 def main():
-        # Define input text
-        text = "3 + 4 * 2 - 1"
 
+    VN = {'S', 'A', 'B', 'C'}
+    VI = {'a', 'd'}
+    P = [
+        ('S', ('d', 'B')),
+        ('S', ('A',)),
+        ('A', ('d',)),
+        ('A', ('d', 'S')),
+        ('A', ('a', 'B', 'd', 'A', 'B')),
+        ('B', ('a',)),
+        ('B', ('d', 'A')),
+        ('B', ('A',)),
+        ('B', ()),
+        ('C', ('A', 'a')),
+    ]
 
-        # Create lexer object
-        lexer = Lexer(text)
+    S = 'S'
+    grammar = (VN, VI, P, S)
 
-        token = lexer.get_next_token()
+    # Convert the grammar to Chomsky normal form
+    cnf_converter = CNFConverter(grammar)
+    cnf_grammar = cnf_converter.convert_to_cnf()
 
-        while token.type != "EOF":
-            print(token)
-            token = lexer.get_next_token()
+    # Print the resulting grammar
+    print('Original grammar:')
+    print(grammar)
+    print('Grammar in Chomsky normal form:')
+    print(cnf_grammar)
 
-        print(token)
+    print("All Tests Passed")
 
 if __name__ == '__main__':
     main()
